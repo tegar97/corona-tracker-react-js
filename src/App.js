8a901fcae67c641082fd6221c2ba8d91d3b97a1b
Muhammad Tegar Akmal <eategar@gmail.com>
2020-07-14 13:35:34 +0700

    integrate api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react'
-
-
 import {Cards,Chart,CountryPicker} from './components'
+
 import styles from './App.module.css'
+
+import {fetchData} from './Api/index'
+
 class App extends React.Component{
+    async componentDidMount() {
+        const featchData = await fetchData()
+        this.setState({data : featchData })
+
+    }
     render() {
+        const {data } = this.state
         return(
             <div className={styles.container}>
-                <Chart />
+                <Cards data={data} />
             </div>
         )
     }
