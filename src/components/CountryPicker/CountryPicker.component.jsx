ed70b97c68a7b715cccb9b3a11ea2d496bd864b5
Muhammad Tegar Akmal <eategar@gmail.com>
2020-07-14 17:04:27 +0700

    add country picker api

--- src/components/CountryPicker/CountryPicker.component.jsx
+++ src/components/CountryPicker/CountryPicker.component.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import {NativeSelect,FormControl} from '@material-ui/core'
+
+import styles from './CountryPicker.component'
+
+import {countries} from '../../api';
 
 const Chart = () => {
+    const {fetchcountries,setFechedCountries} = useState([])
+    useEffect(() => {
+        const fetchcountries = async () => {
+            setFechedCountries(await countries)
+
+        }
+        fetchcountries()
+    })
     return (
-        <h1>Country Picker</h1>
+       <FormControl className={styles.FormControl}>
+         <NativeSelect>
+            <option value="global"></option>
+         </NativeSelect>
+       </FormControl>
     )
 }
 
